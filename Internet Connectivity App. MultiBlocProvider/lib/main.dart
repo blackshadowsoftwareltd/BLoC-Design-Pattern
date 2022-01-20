8311f4d0aa5156c0f70d8d525d8bf3b257f3cc25
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2022-01-21 00:05:08 +0600

    _

--- Internet Connectivity App. MultiBlocProvider/lib/main.dart
+++ Internet Connectivity App. MultiBlocProvider/lib/main.dart
@@ -37,54 +37,52 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     /// BlocListener only listen. not rebuild states
     return BlocListener<InternetCubit, InternetState>(
-      listener: (context, state) {
-        /// It will increment or decrement when the connection type will be changed
-        if (state is InternetConnected &&
-            state.connectionType == ConnectionType.wifi) {
-          BlocProvider.of<CounterCubit>(context).increment();
-        } else if (state is InternetConnected &&
-            state.connectionType == ConnectionType.mobile) {
-          BlocProvider.of<CounterCubit>(context).decrement();
-        }
-      },
-      child: Scaffold(
-        body: Center(
-
-            /// BlocBuilder
-            child: BlocBuilder<InternetCubit, InternetState>(
-                builder: (context, state) {
-          /// InternetDisconnected
-          if (state is InternetDisconnected) {
-            return const Text('Offline', style: TextStyle(fontSize: 25));
+        listener: (context, state) {
+          /// It will increment or decrement when the connection type will be changed
+          if (state is InternetConnected &&
+              state.connectionType == ConnectionType.wifi) {
+            BlocProvider.of<CounterCubit>(context).increment();
+          } else if (state is InternetConnected &&
+              state.connectionType == ConnectionType.mobile) {
+            BlocProvider.of<CounterCubit>(context).decrement();
           }
+        },
+        child: Scaffold(
+            body: Center(
 
-          /// InternetConnected
-          if (state is InternetConnected) {
-            return Text(
-                state.connectionType == ConnectionType.wifi
-                    ? 'Connection type WiFe'
-                    : state.connectionType == ConnectionType.mobile
-                        ? 'Connection type Mobile'
-                        : state.connectionType == ConnectionType.ethernet
-                            ? 'Connection type Ethernet'
-                            : state.connectionType == ConnectionType.offline
-                                ? 'Connection type Offline'
-                                : '',
-                style: const TextStyle(fontSize: 25));
-          }
-          return const Center(child: CircularProgressIndicator());
-        })),
-        floatingActionButton: FloatingActionButton(
-          child: BlocBuilder<CounterCubit, CounterState>(
-            /// Counter value
-            builder: (context, state) => Text(
-              state.counterValue.toString(),
-              style: const TextStyle(fontSize: 30),
-            ),
-          ),
-          onPressed: null,
-        ),
-      ),
-    );
+                /// BlocBuilder
+                child: BlocBuilder<InternetCubit, InternetState>(
+                    builder: (context, state) {
+              /// InternetDisconnected
+              if (state is InternetDisconnected) {
+                return const Text('Offline', style: TextStyle(fontSize: 25));
+              }
+
+              /// InternetConnected
+              if (state is InternetConnected) {
+                return Text(
+                    state.connectionType == ConnectionType.wifi
+                        ? 'Connection type WiFe'
+                        : state.connectionType == ConnectionType.mobile
+                            ? 'Connection type Mobile'
+                            : state.connectionType == ConnectionType.ethernet
+                                ? 'Connection type Ethernet'
+                                : state.connectionType == ConnectionType.offline
+                                    ? 'Connection type Offline'
+                                    : '',
+                    style: const TextStyle(fontSize: 25));
+              }
+              return const Center(child: CircularProgressIndicator());
+            })),
+
+            ///
+            floatingActionButton: FloatingActionButton(
+                child: BlocBuilder<CounterCubit, CounterState>(
+
+                    /// Counter value
+                    builder: (context, state) => Text(
+                        state.counterValue.toString(),
+                        style: const TextStyle(fontSize: 30))),
+                onPressed: null)));
   }
 }
